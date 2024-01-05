98cdb66879275fbd2f6fff37c072fe5e9a6deba0
Avielni222 <155298746+Avielni222@users.noreply.github.com>
2024-01-05 18:20:32 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -7,6 +7,9 @@ public class DamkaBoard {
 		int x = Integer.parseInt(args[0]);
 		int i;
 		int j;
+		/* you are not supposed to define int i,j outside of the for 
+		correct way: for( int i = 0;i < x; i++) for( int j=0;...) 
+  		so that i and j are defined only within the scope of the for loops */
 		for (i = 0; i < x; i++)
 		{
 			for (j = 0; j < x; j++)
